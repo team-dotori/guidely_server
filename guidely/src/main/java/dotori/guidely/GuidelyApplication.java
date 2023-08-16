428700010a0d09e4d449beea7886a957d8d5de44
Jieon <dlwldjs7544@naver.com>
2023-08-16 14:48:06 +0900

    feat: 프로젝트 개발 환경 세팅

---
+++ guidely/src/main/java/dotori/guidely/GuidelyApplication.java
@@ -0,0 +1,13 @@
+package dotori.guidely;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuidelyApplication.class, args);
+	}
+
+}
