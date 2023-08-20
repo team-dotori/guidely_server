97d88f3ae58182987c027087d87bfd149b59764e
Jieon <dlwldjs7544@naver.com>
2023-08-20 15:39:58 +0900

    feat: CORS 설정 & 추후 config 파일로 refactoring

--- guidely/src/main/java/dotori/guidely/GuidelyApplication.java
+++ guidely/src/main/java/dotori/guidely/GuidelyApplication.java
@@ -2,12 +2,26 @@ package dotori.guidely;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GuidelyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GuidelyApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://172.30.1.67");
+			}
+		};
 	}
 
 }
