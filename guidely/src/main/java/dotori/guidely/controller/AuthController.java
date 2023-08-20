5798fc1062196f8fa651ab27c97a3e885b9c112a
Jieon <dlwldjs7544@naver.com>
2023-08-20 15:41:27 +0900

    refactor: authorizationCode 받기 수정

--- guidely/src/main/java/dotori/guidely/controller/AuthController.java
+++ guidely/src/main/java/dotori/guidely/controller/AuthController.java
@@ -3,6 +3,7 @@ package dotori.guidely.controller;
 import dotori.guidely.domain.oauth.AuthTokens;
 import dotori.guidely.domain.oauth.kakao.KakaoLoginParams;
 import dotori.guidely.service.OAuthLoginService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
@@ -24,10 +25,21 @@ public class AuthController {
 //
 //    }
 
-    @PostMapping("/api/auth/kakao")
-    public ResponseEntity<AuthTokens> loginKakao(@RequestBody KakaoLoginParams params) {
+//    @GetMapping("/kakao/callback")
+//    public String getAuthorizationCode(@RequestParam("code") String code) {
+//        System.out.println("code = " + code);
+//        return code;
+//    }
+//
+//    @PostMapping("/api/auth/kakao")
+//    public ResponseEntity<AuthTokens> loginKakao(@RequestBody KakaoLoginParams params) {
+//        return ResponseEntity.ok(oAuthLoginService.login(params));
+//    }
+
+    @GetMapping("/kakao/callback")
+    public ResponseEntity<AuthTokens> loginKakao(@RequestParam("code") String code) {
+        KakaoLoginParams params = new KakaoLoginParams(code);
+
         return ResponseEntity.ok(oAuthLoginService.login(params));
     }
-
-
 }
