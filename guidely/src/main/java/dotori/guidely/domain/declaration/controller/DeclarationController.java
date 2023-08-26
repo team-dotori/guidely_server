ef828053bb09c6acddbfd17123b32eca199c7040
heemin <gmlals9628@naver.com>
2023-08-26 17:51:45 +0900

    feat: 신고 정보 모두 가져오기 기능 추가

--- guidely/src/main/java/dotori/guidely/domain/declaration/controller/DeclarationController.java
+++ guidely/src/main/java/dotori/guidely/domain/declaration/controller/DeclarationController.java
@@ -2,12 +2,15 @@ package dotori.guidely.domain.declaration.controller;
 
 import dotori.guidely.domain.declaration.domain.Declaration;
 import dotori.guidely.domain.declaration.dto.DeclarationDto;
+import dotori.guidely.domain.declaration.dto.response.DeclarationResponseDto;
 import dotori.guidely.domain.declaration.service.DeclarationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +26,9 @@ public class DeclarationController {
                 .status(HttpStatus.CREATED)
                 .body(declarationService.saveDeclaration(declarationDto));
     }
-
+    @GetMapping
+    public ResponseEntity<List<DeclarationResponseDto>> findAll(){
+        return ResponseEntity
+                .ok(declarationService.findAll());
+    }
 }
--- guidely/src/main/java/dotori/guidely/domain/declaration/service/DeclarationService.java
+++ guidely/src/main/java/dotori/guidely/domain/declaration/service/DeclarationService.java
@@ -2,12 +2,17 @@ package dotori.guidely.domain.declaration.service;
 
 import dotori.guidely.domain.declaration.domain.Declaration;
 import dotori.guidely.domain.declaration.dto.DeclarationDto;
+import dotori.guidely.domain.declaration.dto.response.DeclarationResponseDto;
 import dotori.guidely.domain.declaration.repository.DeclarationRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class DeclarationService {
@@ -19,5 +24,13 @@ public class DeclarationService {
         return declarationRepository.save(declarationDto.toEntity());
     }
 
-
+    public List <DeclarationResponseDto> findAll(){
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        List<Declaration> declarations = declarationRepository.findAll();
+        List<DeclarationResponseDto> responseDtos = new ArrayList<>();
+        for(Declaration declaration : declarations){
+            responseDtos.add(modelMapper.map(declaration, DeclarationResponseDto.class));
+        }
+        return responseDtos;
+    }
 }
