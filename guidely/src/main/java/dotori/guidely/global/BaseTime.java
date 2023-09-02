bc716ce96f01eedbc2798912683bb11aa16e2ed3
LeeJieon <dlwldjs7544@naver.com>
2023-09-03 03:21:14 +0900

    Refactor: BaseTime @SuperBuilder 추가
    - BaseTime class에 @SuperBuilder 어노테이션 추가.
    - modifedDate 수정 필요
    
    Related to: #12

--- guidely/src/main/java/dotori/guidely/global/BaseTime.java
+++ guidely/src/main/java/dotori/guidely/global/BaseTime.java
@@ -1,19 +1,29 @@
 package dotori.guidely.global;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTime {
+
     @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @LastModifiedDate
