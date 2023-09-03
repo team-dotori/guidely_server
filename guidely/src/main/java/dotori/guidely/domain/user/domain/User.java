9d77b0814c5f8a40a0dc89b43cd4b242207bf04f
Jieon <dlwldjs7544@naver.com>
2023-09-03 15:37:47 +0900

    Refactor: User 도메인 수정, OAuth에서 @Builder 사용
    - User 도메인의 @Data를 삭제하고 @Getter를 사용
    - 따라서 OAuthService에서  @Builder를 사용하여 DTO로 전환
    - 사용자의 게시글 목록과 좋아요 목록 추가
    
    Related to: #12

--- guidely/src/main/java/dotori/guidely/domain/user/domain/User.java
+++ guidely/src/main/java/dotori/guidely/domain/user/domain/User.java
@@ -1,27 +1,27 @@
 package dotori.guidely.domain.user.domain;
 
-
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import dotori.guidely.domain.declaration.domain.Declaration;
+import dotori.guidely.domain.heart.domain.Heart;
 import dotori.guidely.domain.oauth.domain.OAuthProvider;
+import dotori.guidely.domain.post.domain.Post;
+import dotori.guidely.global.BaseTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.format.annotation.DateTimeFormat;
+import lombok.experimental.SuperBuilder;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
 @Entity
+@Getter
 @Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
+@SuperBuilder
 public class User extends BaseTime {
 
     @Id
@@ -40,16 +40,16 @@ public class User extends BaseTime {
     @Enumerated(EnumType.STRING)
     private OAuthProvider oAuthProvider;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Post> posts;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Heart> hearts;
+
+    @Builder.Default
     @OneToMany(mappedBy = "user")
     @JsonManagedReference
-    private List<Declaration> declarationList=new ArrayList<>(); ;
-
-    @Builder
-    public User(String email, String nickname, OAuthProvider oAuthProvider) {
-        this.email = email;
-        this.nickname = nickname;
-        this.oAuthProvider = oAuthProvider;
-    }
+    private List<Declaration> declarationList = new ArrayList<>();
 
     public void addDeclaration(Declaration declaration){ //편의 메소드
         this.declarationList.add(declaration);
