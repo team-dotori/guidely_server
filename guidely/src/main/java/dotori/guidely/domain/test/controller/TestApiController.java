23d6fa8db97150e034793f56729160e020052102
LeeJieon <dlwldjs7544@naver.com>
2023-09-05 03:02:02 +0900

    Feat: Test용 Controllerr

---
+++ guidely/src/main/java/dotori/guidely/domain/test/controller/TestApiController.java
@@ -0,0 +1,28 @@
+package dotori.guidely.domain.test.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tests")
+public class TestApiController {
+
+    @GetMapping("/hello")
+    public HelloResponse getHello(){
+        return new HelloResponse(2L, "Hello World V21");
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class HelloResponse{
+        private Long id;
+        private String content;
+    }
+}
